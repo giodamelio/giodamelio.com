9aa0a234dca7244a3f1f6051b5f0fe706e98dd3e
Gio d'Amelio <giodamelio@gmail.com>
2015-09-15 15:07:28 -0700

    Create script to publish to ipfs

---
+++ publish.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+printf "Removing old site... "
+rm -r public/
+echo -e "\t\tDone"
+
+printf "Building Site... "
+time=$(hugo | tail -n 1 | cut -d " " -f 2-3)
+echo -e "\t\tDone (in $time)"
+
+printf "Publishing to IPFS... "
+hash=$(ipfs add -qr public/ | tail -n 1)
+echo -e "\t\tDone"
+
+printf "Updating IPNS... "
+peerid=$(ipfs name publish $hash | cut -d " " -f 3)
+echo -e "\t\tDone"
+
+T="$(printf '\t')"
+
+# Print message
+cat << EOF
+
+-----------------
+Publish sucessfull
+
+IPFS hash:$T$T $hash
+IPNS peerid:$T$T $peerid
+
+IPFS:
+  Local Gateway:$T http://localhost:8080/ipfs/$hash
+  Internet Gateway:$T https://gateway.ipfs.io/ipfs/$hash
+
+IPNS:
+  Local Gateway:$T http://localhost:8080/ipns/$peerid
+  Internet Gateway:$T https://gateway.ipfs.io/ipns/$peerid
+EOF
+
