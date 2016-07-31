519775fb8232166b41154155feb48ebd93de4eb2
Gio d'Amelio <giodamelio@gmail.com>
2016-07-31 13:34:12 -0700

    Render templates with gulp-layout and nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require('gulp');
 const markdownIt = require('gulp-markdown-it');
 const browserSync = require('browser-sync').create();
+const layout = require('gulp-layout');
 
 gulp.task('blog', () => {
   return gulp.src('content/blog/*.md')
     .pipe(markdownIt())
+    .pipe(layout({
+      layout: './layouts/base.html',
+      engine: 'nunjucks',
+    }))
     .pipe(gulp.dest('out/blog/'));
 });
 
