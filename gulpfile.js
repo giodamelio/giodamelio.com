3154194a1439273891c51865a863dafa50902902
Gio d'Amelio <giodamelio@gmail.com>
2016-07-31 13:11:29 -0700

    Serve files with browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 const gulp = require('gulp');
 const markdownIt = require('gulp-markdown-it');
+const browserSync = require('browser-sync').create();
 
 gulp.task('blog', () => {
   return gulp.src('content/blog/*.md')
     .pipe(markdownIt())
     .pipe(gulp.dest('out/blog/'));
 });
+
+// Serve files and auto reload with browser sync
+gulp.task('server', () => {
+  browserSync.init({
+    files: [
+      'content/**/*',
+    ],
+    server: {
+      baseDir: './out',
+      directory: true,
+    },
+    open: false,
+  });
+});
+
+gulp.task('default', ['blog', 'server']);
