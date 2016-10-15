84135a3b37cb059ed9d05ada9ef22f7108859282
Gio d'Amelio <giodamelio@gmail.com>
2016-10-15 12:03:31 -0700

    Add radium with basic example

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Match, Miss, Link } from 'react-router';
+import radium from 'radium';
 
 import { Home, Test, Error404 } from './pages';
+
+const styles = {
+  background: 'red',
+};
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div style={[styles]}>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/test">Test</Link></li>
@@ -21,4 +26,4 @@ function App() {
   );
 }
 
-export default App;
+export default radium(App);
