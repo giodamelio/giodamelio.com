c446554b60929169778280a03d8a520d5305dcb0
Gio d'Amelio <giodamelio@gmail.com>
2016-10-14 23:49:00 -0700

    React router hello world

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Match, Miss, Link } from 'react-router';
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function Test() {
+  return <h1>Test</h1>;
+}
+
+function Error404() {
+  return <h1>Error: 404</h1>;
+}
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/test">Test</Link></li>
+        </ul>
+
+        <Match exactly pattern="/" component={Home} />
+        <Match pattern="/test" component={Test} />
+
+        <Miss component={Error404} />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
