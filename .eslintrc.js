c37cbc3af69e6384ac7ccdc834124b5bd6286447
Gio d'Amelio <giodamelio@gmail.com>
2016-10-15 11:58:39 -0700

    Add some settings to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,16 @@ module.exports = {
       'newlines-between': 'always',
     }],
   },
+  settings: {
+    // Make sure the import plugin knows were to look for modules
+    'import/resolver': {
+      node: {
+        paths: [__dirname],
+      },
+    },
+    // Get more specific react errors
+    react: {
+      version: '15.0',
+    },
+  },
 };
