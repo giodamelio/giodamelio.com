16837253cffae31c156f112be27ad4c7ba9eb17f
Gio d'Amelio <giodamelio@gmail.com>
2017-06-03 23:28:30 -0700

    Add basic auto generated navbar.

---
+++ assets/navbar.js
@@ -0,0 +1,22 @@
+export default {
+  brand: {
+    text: "Gio d'Amelio",
+    url: '/',
+  },
+  links: [
+    {
+      text: 'Home',
+      url: '/',
+    },
+    {
+      text: 'TEST',
+      url: '/test',
+    },
+  ],
+  social: [
+    {
+      icon: 'github',
+      url: 'https://github.com/giodamelio',
+    },
+  ],
+};
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,52 @@
 <template>
   <div>
+    <nav class="nav">
+      <div class="nav-left">
+        <!-- Brand -->
+        <nuxt-link class="nav-item" :to="navbar.brand.url">
+          <strong>
+            {{ navbar.brand.text }}
+          </strong>
+        </nuxt-link>
+
+        <!-- Links -->
+        <nuxt-link class="nav-item"
+                   v-for="link in navbar.links"
+                   :to="link.url"
+                   :key="link.url">
+          {{ link.text }}
+        </nuxt-link>
+      </div>
+
+      <div class="nav-right">
+        <!-- Social -->
+        <a class="nav-item"
+           v-for="site in navbar.social"
+           target="_blank"
+           rel="noopener noreferrer"
+           :href="site.url"
+           :key="site.url">
+          <i :class="'fa fa-2x fa-' + site.icon" aria-hidden="true"></i>
+        </a>
+      </div>
+    </nav>
+
     <nuxt/>
   </div>
 </template>
+
+<script>
+// eslint-disable-next-line
+import navbar from '~assets/navbar';
+
+export default {
+  data() {
+    return {
+      navbar,
+    };
+  },
+};
+</script>
 
 <style>
 </style>
