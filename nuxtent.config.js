543ae6984709d038f57905e43d74414181878dc7
Gio d'Amelio <giodamelio@gmail.com>
2017-08-31 13:54:05 -0700

    Nuxtent generate

---
+++ nuxtent.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: {
+    permalink: ':slug',
+    page: '/_content',
+    generate: [
+      // for static build
+      'get',
+      'getAll',
+    ],
+    isPost: false,
+  },
+
+  api: {
+    baseURL: process.env.NODE_ENV === 'production'
+      ? 'http://localhost:3000'
+      : 'http://localhost:3000',
+  },
+};
