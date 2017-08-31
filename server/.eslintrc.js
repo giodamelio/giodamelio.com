d674f95c35e652de3a56f5c2df2c9389e7cec174
Gio d'Amelio <giodamelio@gmail.com>
2017-08-31 12:13:17 -0700

    Remove server

--- server/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['@giodamelio/thehelpining/node'],
-  rules: {
-    'no-process-env': 'off',
-    'no-process-exit': 'off',
-    'thehelp/no-mutation': 'off',
-    'arrow-parens': 'off',
-  },
-};
