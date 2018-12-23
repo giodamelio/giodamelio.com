693bbd2b3e1648a68143e8e1446253a7126ac2ff
Gio d'Amelio <giodamelio@gmail.com>
2018-12-22 20:12:51 -0800

    Add bulma, use it to rebuild the navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,76 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
+import classnames from 'classnames';
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'indianred',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+const NavbarLink = ({ children, ...props }) => (
+  <Link className="navbar-item" {...props}>
+    {children}
+  </Link>
 );
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
+class Header extends Component {
+  static propTypes = {
+    siteTitle: PropTypes.string,
+  };
 
-Header.defaultProps = {
-  siteTitle: '',
-};
+  static defaultProps = {
+    siteTitle: '',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isBurgerMenuOpen: false,
+    };
+
+    this.toggleBurgerMenu = this.toggleBurgerMenu.bind(this);
+  }
+
+  toggleBurgerMenu() {
+    this.setState(state => ({
+      isBurgerMenuOpen: !state.isBurgerMenuOpen,
+    }));
+  }
+
+  render() {
+    const { siteTitle } = this.props;
+    return (
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <NavbarLink to="/">{siteTitle}</NavbarLink>
+
+          <a
+            role="button"
+            className={classnames('navbar-burger', 'burger', {
+              'is-active': this.state.isBurgerMenuOpen,
+            })}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarMain"
+            onClick={this.toggleBurgerMenu}
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
+        </div>
+
+        <div
+          id="navbarMain"
+          className={classnames('navbar-menu', {
+            'is-active': this.state.isBurgerMenuOpen,
+          })}
+        >
+          <div className="navbar-start">
+            <NavbarLink to="/">Home</NavbarLink>
+            <NavbarLink to="/blog">Blog</NavbarLink>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Header;
