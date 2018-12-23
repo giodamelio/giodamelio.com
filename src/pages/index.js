dbc2238e0d9eda79fb6cda1dd044e8a9a5c50ffb
Gio d'Amelio <giodamelio@gmail.com>
2018-12-22 20:14:13 -0800

    Change homepage contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,10 @@ import SEO from '../components/seo';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <h1>Hi people!</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
+    <h2>
+      Some things will eventually be here. You should come back every day and
+      check.
+    </h2>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
