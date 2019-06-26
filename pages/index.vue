bf7ed67da66c0263b62c408aeddd99a45c3dfdd8
Gio d'Amelio <giodamelio@gmail.com>
2019-06-26 00:39:14 +0000

    Add under construction message

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,19 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card title="test" icon="exclamation">
-        TESTING
-      </card>
-
-      <card title="Free" icon="github-circle">
-        Open source on
-        <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey">
-          Every
-        </b>
-        component is responsive
-      </card>
-
-      <card title="Modern" icon="alert-decagram">
-        Built with
-        <a href="https://vuejs.org/">
-          Vue.js
-        </a>
-        and
-        <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">Gio's Internet Braindump</h1>
+        <h2 class="subtitle">
+          Some things will be here eventually. You should come back and check
+          every day.
+        </h2>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
 export default {
-  name: 'HomePage',
-
-  components: {
-    Card
-  }
+  name: 'HomePage'
 }
 </script>
