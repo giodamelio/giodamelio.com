f198e841235236daa54443812513d3d818db18e7
Gio d'Amelio <giodamelio@gmail.com>
2019-06-26 00:52:02 +0000

    Make construction message better

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav
-      class="navbar header has-shadow is-primary"
+      class="navbar header has-shadow is-info"
       role="navigation"
       aria-label="main navigation"
     >
@@ -18,24 +18,7 @@
       </div>
     </nav>
 
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <nuxt-link :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-    </section>
+    <nuxt />
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hero is-primary">
+  <section class="hero is-primary is-fullheight-with-navbar">
     <div class="hero-body">
       <div class="container">
         <h1 class="title">Gio's Internet Braindump</h1>
