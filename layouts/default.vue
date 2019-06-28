2dd970c921b98f4a1a5a9c43aef12250414bd4ba
Gio d'Amelio <giodamelio@gmail.com>
2019-06-28 02:03:56 +0000

    Basic navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="container">
+    <nav class="topnav">
+      <ol>
+        <li v-for="(item, key) of links" :key="key">
+          <a v-if="item.url" :href="item.url">{{ item.text }}</a>
+          <nuxt-link v-if="item.link" :to="item.link">
+            {{ item.text }}
+          </nuxt-link>
+          -
+        </li>
+      </ol>
+    </nav>
     <nuxt />
   </div>
 </template>
@@ -8,11 +19,28 @@
 export default {
   data() {
     return {
+      links: [
+        {
+          text: "Gio d'Amelio",
+          url: 'https://giodamelio.com'
+        },
+        {
+          text: 'Blog',
+          link: '/blog'
+        },
+        {
+          text: 'Style Text',
+          link: '/style-test'
+        }
+      ],
       social: [
         {
-          title: 'Github',
-          icon: 'github-circle',
+          text: 'Github',
           url: 'https://github.com/giodamelio'
+        },
+        {
+          text: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/giodamelio'
         }
       ]
     };
