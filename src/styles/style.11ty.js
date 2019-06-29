f772b764332eec083739ccae9346bc233e86b086
Gio d'Amelio <giodamelio@gmail.com>
2019-06-29 02:57:36 +0000

    Setup css preprocessing with postcss

---
+++ src/styles/style.11ty.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const postcss = require('postcss');
+const functions = require('postcss-functions');
+
+const filename = 'markdown.css';
+
+const functionsOptions = {
+  functions: {
+    repeatChar(char, count) {
+      return `'${char.repeat(count)}'`;
+    }
+  }
+};
+
+module.exports = class {
+  async data() {
+    const rawFilepath = path.join(__dirname, `${filename}`);
+    return {
+      permalink: `static/css/${filename}`,
+      rawFilepath,
+      rawCss: await fs.readFileSync(rawFilepath)
+    };
+  }
+
+  async render({ rawCss, rawFilepath }) {
+    return await postcss([require('precss'), functions(functionsOptions)])
+      .process(rawCss, { from: rawFilepath })
+      .then(res => {
+        // console.log(res);
+        return res.css;
+      });
+  }
+};
