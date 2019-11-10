a48249c5df8ebe421dbf7665a6c1f23bdfe6b293
Gio d'Amelio <giodamelio@gmail.com>
2019-11-10 05:52:38 +0000

    Set the meta theme color

--- src/main.js
+++ src/main.js
@@ -6,4 +6,10 @@ import DefaultLayout from '~/layouts/Default.vue';
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
+
+  // Set some meta tags
+  head.meta.push({
+    name: 'theme-color',
+    content: '#2c5282' // blue-900 from TailwindCSS
+  });
 }
