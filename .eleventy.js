ffc96f9c6e9c3e5bc2d2cb00e95dcb685f2df491
Gio d'Amelio <giodamelio@gmail.com>
2021-07-11 00:02:57 +0000

    Use normalize.css in our page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
-module.exports = function (_eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/static/');
+
   return {
     dir: {
       input: 'src/',
